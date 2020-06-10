d9fc36232606d0e0b3f78cec49f4623211e4a40d
Katie Carroll <ktcarroll4@gmail.com>
2020-06-10 11:31:30 -0500

    exercises 1-2

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export default {
+  getItems
+};
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/katie&MattP';
+
+function getItems() {
+  return Promise.resolve('A successful response!');
+}
+
